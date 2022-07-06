bfafb0bff1cec8df97263af2fe707bd7ebd6b00c
duanfuxing <duanfuxing@eventmosh.com>
2022-07-06 16:11:57 +0800

    update

--- src/Api/BaseRequest.php
+++ src/Api/BaseRequest.php
@@ -10,6 +10,8 @@ use Psr\SimpleCache\InvalidArgumentException;
 class BaseRequest
 {
     public const OAUTH_CACHE_KEY = 'doudian_oauth_token';
+    public const OAUTH_CACHE_EXPIRED = 60 * 24; // 分钟 一天
+
     /**
      * @var array 配置参数
      */
@@ -30,11 +32,11 @@ class BaseRequest
     {
         $this->config = $config;
         $this->shop_id = $shop_id;
-        if (! isset($config['app_key']) || ! $config['app_key']) {
+        if (!isset($config['app_key']) || !$config['app_key']) {
             throw new \InvalidArgumentException('配置有误, 请填写app_key');
         }
 
-        if (! isset($config['app_secret']) || ! $config['app_secret']) {
+        if (!isset($config['app_secret']) || !$config['app_secret']) {
             throw new \InvalidArgumentException('配置有误, 请填写app_secret');
         }
         $this->client = new Client();
@@ -44,13 +46,13 @@ class BaseRequest
      * 发起GET请求
      *
      * @param string $url
-     * @param array  $params
-     * @param bool   $needSign
-     *
-     * @throws RequestException
-     * @throws InvalidArgumentException
+     * @param array $params
+     * @param bool $needSign
      *
      * @return array
+     * @throws InvalidArgumentException
+     *
+     * @throws RequestException
      */
     public function httpGet(string $url, array $params = [], bool $needSign = true): array
     {
@@ -62,13 +64,13 @@ class BaseRequest
      *
      * @param string $method
      * @param string $url
-     * @param array  $params
-     * @param bool   $needSign
-     *
-     * @throws RequestException
-     * @throws InvalidArgumentException
+     * @param array $params
+     * @param bool $needSign
      *
      * @return array
+     * @throws InvalidArgumentException
+     *
+     * @throws RequestException
      */
     private function request(string $method, string $url, array $params = [], bool $needSign = true): array
     {
@@ -82,7 +84,7 @@ class BaseRequest
         $key = $method == 'get' ? 'query' : 'form_params';
         $options[$key] = $params;
 
-        $response = $this->client->request($method, $this->baseUrl.$url, $options);
+        $response = $this->client->request($method, $this->baseUrl . $url, $options);
 
         return json_decode($response->getBody()->getContents(), true);
     }
@@ -91,42 +93,42 @@ class BaseRequest
      * 组合请求参数.
      *
      * @param string $url
-     * @param array  $params
-     *
-     * @throws RequestException
-     * @throws InvalidArgumentException
+     * @param array $params
      *
      * @return array
+     * @throws InvalidArgumentException
+     *
+     * @throws RequestException
      */
     protected function generateParams(string $url, array $params): array
     {
         $url = str_replace('/', '.', $url);
 
         $accessToken = "";
-        if (! in_array($url, ["token.create","token.refresh"])) {
+        if (!in_array($url, ["token.create", "token.refresh"])) {
             $accessToken = $this->getAccessToken();
         };
 
         $public = [
-            'app_key' => $this->config['app_key'],
-            'timestamp' => date('Y-m-d H:i:s'),
-            'v' => '2',
-            'method' => $url,
+            'app_key'      => $this->config['app_key'],
+            'timestamp'    => date('Y-m-d H:i:s'),
+            'v'            => '2',
+            'method'       => $url,
             'access_token' => $accessToken,
         ];
 
         ksort($params);
-        $param_json = json_encode((object) $params, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        $param_json = json_encode((object)$params, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
-        $str = 'app_key'.$public['app_key'].'method'.$url.'param_json'.$param_json.'timestamp'.$public['timestamp'].'v'.$public['v'];
-        $md5_str = $this->config['app_secret'].$str.$this->config['app_secret'];
+        $str = 'app_key' . $public['app_key'] . 'method' . $url . 'param_json' . $param_json . 'timestamp' . $public['timestamp'] . 'v' . $public['v'];
+        $md5_str = $this->config['app_secret'] . $str . $this->config['app_secret'];
         $sign = md5($md5_str);
 
         return array_merge(
             $public,
             [
                 'param_json' => $param_json,
-                'sign' => $sign,
+                'sign'       => $sign,
             ]
         );
     }
@@ -134,15 +136,15 @@ class BaseRequest
     /**
      * 获取TOKEN.
      *
-     * @throws RequestException
+     * @return mixed|string
      * @throws InvalidArgumentException
      *
-     * @return mixed|string
+     * @throws RequestException
      */
     private function getAccessToken(): string
     {
-        $oauthToken = Cache::get(self::OAUTH_CACHE_KEY.$this->shop_id, []);
-        if (! $oauthToken || ! $oauthToken['refresh_token']) {
+        $oauthToken = Cache::get(self::OAUTH_CACHE_KEY . $this->shop_id, []);
+        if (!$oauthToken || !$oauthToken['refresh_token']) {
             return $this->requestAccessToken();
         }
 
@@ -156,15 +158,15 @@ class BaseRequest
     /**
      * 请求TOKEN.
      *
-     * @throws RequestException
+     * @return string
      * @throws InvalidArgumentException
      *
-     * @return string
+     * @throws RequestException
      */
     private function requestAccessToken(): string
     {
         $param = [
-            'app_id' => $this->config['app_key'],
+            'app_id'     => $this->config['app_key'],
             'app_secret' => $this->config['app_secret'],
             'grant_type' => 'authorization_self',
         ];
@@ -174,13 +176,13 @@ class BaseRequest
         }
 
         $response = $this->httpGet('token/create', $param, true);
-        if (! $response || $response['code'] > 10000) {
+        if (!$response || $response['code'] > 10000) {
             trigger_error("token/create 接口异常[{$response['code']}]");
         }
         $response['data']['access_token_expired_at'] = time() + $response['data']['expires_in'];
         $response['data']['refresh_token_expired_at'] = strtotime('+14 day');
 
-        Cache::set(self::OAUTH_CACHE_KEY.$this->shop_id, $response['data']);
+        Cache::add(self::OAUTH_CACHE_KEY . $this->shop_id, $response['data']);
 
         return $response['data']['access_token'];
     }
@@ -190,28 +192,28 @@ class BaseRequest
      *
      * @param string $refreshToken
      *
-     * @throws RequestException
+     * @return string
      * @throws InvalidArgumentException
      *
-     * @return string
+     * @throws RequestException
      */
     private function updateAccessToken(string $refreshToken): string
     {
         $param = [
-            'app_id' => $this->config['app_key'],
-            'app_secret' => $this->config['app_secret'],
-            'grant_type' => 'refresh_token',
+            'app_id'        => $this->config['app_key'],
+            'app_secret'    => $this->config['app_secret'],
+            'grant_type'    => 'refresh_token',
             'refresh_token' => $refreshToken,
         ];
 
         $response = $this->httpGet('token/refresh', $param, true);
-        if (! $response || $response['code'] > 10000) {
+        if (!$response || $response['code'] > 10000) {
             trigger_error("token/refresh 接口异常[{$response['code']}]");
         }
         $response['data']['access_token_expired_at'] = time() + $response['data']['expires_in'];
         $response['data']['refresh_token_expired_at'] = strtotime('+14 day');
 
-        Cache::set(self::OAUTH_CACHE_KEY.$this->shop_id, $response['data']);
+        Cache::add(self::OAUTH_CACHE_KEY . $this->shop_id, $response['data']);
 
         return $response['data']['access_token'];
     }
@@ -220,13 +222,13 @@ class BaseRequest
      * 发起POST请求
      *
      * @param string $url
-     * @param array  $params
-     * @param bool   $needSign
-     *
-     * @throws RequestException
-     * @throws InvalidArgumentException
+     * @param array $params
+     * @param bool $needSign
      *
      * @return array
+     * @throws InvalidArgumentException
+     *
+     * @throws RequestException
      */
     public function httpPost(string $url, array $params = [], bool $needSign = true): array
     {
